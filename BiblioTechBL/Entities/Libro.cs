5acb2e3aa5050254a88aef4d67f61968d4b73f1b
Gianluca Guariento <E-gianluca.guariento@msc.com>
2023-02-01 12:21:31 +0100

    Inserimenti classi Libro e Prestito

--- BiblioTechBL/Entities/Libro.cs
+++ BiblioTechBL/Entities/Libro.cs
@@ -8,5 +8,32 @@ namespace BiblioTechBL.Entities
 {
     public class Libro
     {
+      
+
+        public int Id { get; private set; }
+       
+        public string Nome { get; private set; }
+       
+        public string Autore { get; private set; }
+       
+        public DateOnly AnnoPubblicazione { get; private set; }
+        
+        public string Genere { get; private set; }
+
+        public bool IsAvailable { get; private set; }
+
+
+        public Libro(int id, string nome, string autore, DateOnly annoPubblicazione, string genere, bool isAvailable)
+        {
+            Id = id;
+            Nome = nome;
+            Autore = autore;
+            AnnoPubblicazione = annoPubblicazione;
+            Genere = genere;
+            IsAvailable = isAvailable;
+        }
+
+
+
     }
 }
