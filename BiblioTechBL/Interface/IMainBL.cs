d18f9ef30e6f7822feb6269e077db7c8bb0ba2c7
Franco Domolo (External) <e-franco.domolo@msc.com>
2023-02-01 02:57:35 -0800

    Pezzi di imainbl

---
+++ BiblioTechBL/Interface/IMainBL.cs
@@ -0,0 +1,17 @@
+﻿using BiblioTechBL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiblioTechBL.Interface
+{
+    public interface IMainBL
+    {
+        Iscritto GetIscrittoById(int id);
+        Libro GetLibroById(int id);
+        Prestito GetPrestitoById(int id);
+    }
+}
